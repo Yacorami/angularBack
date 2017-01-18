cbf0d961f2d0fad2d4c06d32b1d92c269c1b9e37
yacine rami <yacine rami>
2017-01-18 12:22:34 +0000

    first commit backup

--- angular/libs/angular/app/app.js
+++
@@ -1,6 +0,0 @@
-var myApp = angular.module('myApp', ['ui.select','datatables','ngResource','ngDialog']);
-myApp.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-}]);
-
-
